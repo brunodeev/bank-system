e6c1d6f88e7335df58ae0097101506f4da168b2a
Bruno César <bcgmeireles@gmail.com>
2024-10-25 10:56:24 -0300

    feat: initializes project structure

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Login from './pages/Login'
+
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
---
+++ src/pages/Login.jsx
@@ -0,0 +1,17 @@
+export default function Login() {
+    return (
+        <div className="container">
+            <div className="title">
+                <h2>Login</h2>
+            </div>
+
+            <div className="email">
+                <input type="email"/>
+            </div>
+
+            <div className="password">
+                <input type="password"/>
+            </div>
+        </div>
+    );
+}
